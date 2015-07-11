a2031e2a54db66ec2299686f6579acc066a1065b
makotan <makotan@gmail.com>
2015-07-12 06:29:25 +0900

    add tmux zshrc

--- init.el
+++ init.el
@@ -23,6 +23,50 @@
   (package-install 'clojure-mode))
 
 
+(when (require 'helm-config nil t)
+  (helm-mode 1)
+
+  (define-key global-map (kbd "M-x")     'helm-M-x)
+  (define-key global-map (kbd "C-x C-f") 'helm-find-files)
+  (define-key global-map (kbd "C-x C-r") 'helm-recentf)
+  (define-key global-map (kbd "M-y")     'helm-show-kill-ring)
+  (define-key global-map (kbd "C-c i")   'helm-imenu)
+  (define-key global-map (kbd "C-x b")   'helm-buffers-list)
+
+  (define-key helm-map (kbd "C-h") 'delete-backward-char)
+  (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+  (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+  (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+
+  ;; Disable helm in some functions
+  (add-to-list 'helm-completing-read-handlers-alist '(find-alternate-file . nil))
+
+  ;; Emulate `kill-line' in helm minibuffer
+  (setq helm-delete-minibuffer-contents-from-point t)
+  (defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)
+    "Emulate `kill-line' in helm minibuffer"
+    (kill-new (buffer-substring (point) (field-end))))
+
+  (defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
+    "Execute command only if CANDIDATE exists"
+    (when (file-exists-p candidate)
+      ad-do-it))
+
+  (defadvice helm-ff-transform-fname-for-completion (around my-transform activate)
+    "Transform the pattern to reflect my intention"
+    (let* ((pattern (ad-get-arg 0))
+           (input-pattern (file-name-nondirectory pattern))
+           (dirname (file-name-directory pattern)))
+      (setq input-pattern (replace-regexp-in-string "\\." "\\\\." input-pattern))
+      (setq ad-return-value
+            (concat dirname
+                    (if (string-match "^\\^" input-pattern)
+                        ;; '^' is a pattern for basename
+                        ;; and not required because the directory name is prepended
+                        (substring input-pattern 1)
+                      (concat ".*" input-pattern)))))))
+
+
 (require 'popwin)
 (setq display-buffer-function 'popwin:display-buffer)
 
@@ -48,7 +92,7 @@
 (push '(direx:direx-mode :position left :width 40 :dedicated t)
       popwin:special-display-config)
 
-(require 'alchemist)
+;(require 'alchemist)
 (setq alchemist-project-compile-when-needed t)
 
 
@@ -57,4 +101,14 @@
 ;(define-key company-mode-map "\M-h" 'company-complete)
 (setq company-idle-delay 0.3)
 (setq company-minimum-prefix-length 2)
+
+(defun elixir-mode-compile-on-save ()
+  "Elixir mode compile files on save."
+    (and (file-exists (buffer-file-name))
+         (file-exists (elixir-mode-compiled-file-name))
+             (elixir-cos-mode t)))
+(add-hook 'elixir-mode-hook 'elixir-mode-compile-on-save)
+(add-hook 'elixir-mode-hook 'alchemist-mode)
+
+
 
